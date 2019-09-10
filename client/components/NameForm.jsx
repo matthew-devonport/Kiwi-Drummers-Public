1843cd49b7d11e9c01ea52fa4991075e6aaa1124
WillHoughton <will.houghton2000@gmail.com>
2019-09-10 16:04:31 +1200

    Getting data from page and linking API

--- client/components/NameForm.jsx
+++ client/components/NameForm.jsx
@@ -1,38 +1,65 @@
 import React from 'react';
-import BandsForm from './BandsForm'
-import BandsNameForm from './BandsNameForm'
+import { newDrummer } from '../api';
+
 
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 'Please state your full name' };
+    this.state = { numberOfBands: 0};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
-    
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
-    alert('Thank you! ' + this.state.value);
+    alert('Thank you! ' + this.state.name);
     event.preventDefault();
-  
-    
+    const data ={
+      fullName: this.state.name,
+      genre: this.state.genre,
+      active: this.state.active,
+      number: this.state.numberOfBands,
+      bands: this.state.nameOfBands
+
+    }
+    newDrummer(data)
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           <h2>Your full name:</h2>
-          <textarea value={this.state.value} onChange={this.handleChange} />
+          <textarea
+            name='name'
+            value={this.state.name}
+            placeholder='Please enter your name'
+            onChange={this.handleChange}
+          />
         </label>
         <h2>Please select your genre of music:</h2>
         <label>
-          <select value={this.state.value} onChange={this.handleChange}>
+          <select
+            name='genre'
+            value={this.state.genre}
+            onChange={this.handleChange}
+          >
+            <option value=''></option>
             <option value='Metal'>Metal</option>
             <option value='Rock'>Rock</option>
             <option value='Pop'>Pop</option>
@@ -40,8 +67,34 @@ class NameForm extends React.Component {
             <option value='Jazz'>Jazz</option>
           </select>
         </label>
-        <BandsForm />
-       <BandsNameForm />
+        <label>
+          <h2>Are you currently active in a band?</h2>
+          Yes:
+          <input
+            name='Yes'
+            type='checkbox'
+            checked={this.state.active}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          <h2>How many bands are you in?</h2>
+          <input
+            name='numberOfBands'
+            type='number'
+            value={this.state.numberOfBands}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          <h2>Names of the bands you currently drum for:</h2>
+          <textarea
+            name='nameOfBands'
+            value={this.state.nameOfBands}
+            onChange={this.handleChange}
+          />
+        </label>
         <input type='submit' value='submit' />
       </form>
     );
--- seeds/formData.js
+++ seeds/formData.js
@@ -3,7 +3,7 @@ exports.seed = function(knex) {
   return knex('newData').del()
     .then(function () {
       return knex('newData').insert([
-        {id: 1, fullname: '', genre: '', active: undefined, number: undefined, bands: ''} ,
+        {id: 1, fullName: '', genre: '', active: undefined, number: undefined, bands: ''} ,
         
       ]);
     });
